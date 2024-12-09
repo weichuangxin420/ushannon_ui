140a1f38fd0e350e01d96fd7a88b0ed0745a6af0
chuangxinwei <18210880215@163.com>
2024-12-09 18:09:00 +0800

    美化

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,19 +1,16 @@
-#存放装饰器
+# 存放装饰器
+
 
 from functools import wraps
 
-import requests
-from config.env import LLM
-from utils.logger import log
-from datetime import datetime
-from requests.exceptions import HTTPError,ConnectionError,Timeout,RequestException
+from src.utils.logger import log
 
 
 def exception_catcher(func):
     """捕获异常并记录日志的装饰器"""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        page = kwargs.get("page")
         try:
             return func(*args, **kwargs)
         except Exception as e:
@@ -21,31 +18,32 @@ def exception_catcher(func):
             log.error(f"在执行 '{func.__name__}' 时捕获到异常：{e}", exc_info=True)
             # 重新抛出异常以便上层调用者处理
             raise e
+
     return wrapper
 
 
 def logger(func):
     """记录日志的装饰器"""
+
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         log.debug(f"{func.__name__}执行成功")
         return result
+
     return wrapper
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     #
     @logger
     # @UI_exception_catcher
-    def tsum(a,b):
-        return a+b
-    tsum(1,2)
+    def tsum(a, b):
+        return a + b
+
+    tsum(1, 2)
     #
     # @http_exception_catcher
     # def t_http():
     #     respond = requests.get("https://baidu.com")
     #     return respond
     # print(t_http())
-
