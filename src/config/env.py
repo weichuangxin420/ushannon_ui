395ec47528cfaa031599a0cf2d19b30e102ec539
chuangxinwei <18210880215@163.com>
2024-12-09 17:00:00 +0800

    环境变量

---
+++ src/config/env.py
@@ -0,0 +1,19 @@
+#存放环境变量
+
+
+class URLDve:
+
+    LLM = "https://dev-backend.xizi-ai.com"
+    OJ_backend = "https://dev-admin.u-shannon.com/"
+
+class LLM:
+    access_token = None
+    refresh_token = None
+
+
+class Token:
+    token_11_9 = "3b960aa8-754e-4927-b9e9-7aed65c2cf73"
+
+
+class Unclear:
+    user_id = []
