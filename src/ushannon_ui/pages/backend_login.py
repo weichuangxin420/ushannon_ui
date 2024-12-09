0ac29bd594bc0eb9368eb28957b63ca4d253a4a5
chuangxinwei <18210880215@163.com>
2024-12-09 18:08:34 +0800

    美化

--- src/ushannon_ui/pages/backend_login.py
+++ src/ushannon_ui/pages/backend_login.py
@@ -1,7 +1,6 @@
-from config.env import URLDve
-from utils.logger import log
-from utils.decorators import logger,exception_catcher as ec
-
+from src.utils.decorators import exception_catcher as ec
+from src.utils.decorators import logger
+from src.utils.logger import log
 
 
 class LoginPage:
@@ -10,23 +9,17 @@ class LoginPage:
         self.page = page
 
         try:
-            #元素初始化
-            #用户名输入框
+            # 元素初始化
+            # 用户名输入框
             self.input_username = self.page.locator("[type = text]")
-            #密码输入框
+            # 密码输入框
             self.input_password = self.page.locator("[type = password]")
-            #登录按钮
+            # 登录按钮
             self.button_login = self.page.locator("[type = submit]")
-            #备案
-            self.filing  =
-
 
         except Exception as e_login:
             log.error(f"元素定位时发送异常:{self.input_username}")
             raise e_login
-
-
-
 
     @logger
     @ec
@@ -34,34 +27,14 @@ class LoginPage:
         """输入用户名"""
         return self.input_username.fill("admin")
 
-
     @logger
     @ec
     def password_input(self):
         """输入密码"""
         return self.input_password.fill("xz666666")
 
-
     @logger
     @ec
     def login_click(self):
         """点击登录"""
         return self.button_login.click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
