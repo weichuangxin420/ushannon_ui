b6fec23bed0ce22c217b2eeb8b660737f6ba5bfa
chuangxinwei <18210880215@163.com>
2024-12-09 16:55:55 +0800

    装饰器

---
+++ src/utils/decorators.py
@@ -0,0 +1,51 @@
+#存放装饰器
+
+from functools import wraps
+
+import requests
+from config.env import LLM
+from utils.logger import log
+from datetime import datetime
+from requests.exceptions import HTTPError,ConnectionError,Timeout,RequestException
+
+
+def exception_catcher(func):
+    """捕获异常并记录日志的装饰器"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        page = kwargs.get("page")
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            # 记录错误日志，包含函数名称和异常信息
+            log.error(f"在执行 '{func.__name__}' 时捕获到异常：{e}", exc_info=True)
+            # 重新抛出异常以便上层调用者处理
+            raise e
+    return wrapper
+
+
+def logger(func):
+    """记录日志的装饰器"""
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        log.debug(f"{func.__name__}执行成功")
+        return result
+    return wrapper
+
+
+
+
+if __name__ == '__main__':
+    #
+    @logger
+    # @UI_exception_catcher
+    def tsum(a,b):
+        return a+b
+    tsum(1,2)
+    #
+    # @http_exception_catcher
+    # def t_http():
+    #     respond = requests.get("https://baidu.com")
+    #     return respond
+    # print(t_http())
+
