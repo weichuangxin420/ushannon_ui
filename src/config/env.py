d13a1e99bdd94f46eecc5c217c4eef9d9d7f5e66
chuangxinwei <18210880215@163.com>
2024-12-10 14:24:03 +0800

    添加路径常量

--- src/config/env.py
+++ src/config/env.py
@@ -1,10 +1,19 @@
 # 存放环境变量
+import os
+
+
+class Path:
+    # 源代码路径
+    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # 日志路径
+    log_path = os.path.join(base_path, "data", "logs")
 
 
 class URLDve:
 
     LLM = "https://dev-backend.xizi-ai.com"
     OJ_backend = "https://dev-admin.u-shannon.com/"
+    OJ_front = "https://dev-learn.u-shannon.com/login"
 
 
 class LLM:
@@ -18,3 +27,7 @@ class Token:
 
 class Unclear:
     user_id = []
+
+
+if __name__ == "__main__":
+    path = Path()
