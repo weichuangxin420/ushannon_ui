f8a36a2a0c1cb7a2b0ccafe955e95d869f332ac1
chuangxinwei <18210880215@163.com>
2024-12-10 14:24:48 +0800

    添加用例goto

--- src/ushannon_ui/pages/login.py
+++ src/ushannon_ui/pages/login.py
@@ -3,7 +3,7 @@ from src.utils.decorators import logger
 from src.utils.logger import log
 
 
-class Login:
+class LoginPage(object):
     def __init__(self, page):
         self.page = page
 
@@ -16,26 +16,41 @@ class Login:
             # 登录按钮
             self.bt_login = self.page.locator("[type = submit]")
             # 切换手机号登录按钮
-            self.bt_phone_log = page.locator("text = 手机验证码登录")
+            self.bt_phone_log = self.page.locator("text = 手机验证码登录")
+            # 用户协议复选框
+            self.cb_agreement = self.page.locator(".semi-checkbox-inner-display")
+            # 跳转网页：1.用户协议；2.隐私政策；3.备案网页1；4.备案网页2
+            self.a = self.page.locator("div a")
 
         except Exception as e_locator:
             log.error(f"元素定位错误{e_locator}")
-            pass
 
     @logger
     @ec
-    def username_input(self):
+    def username_input(self, content: str):
         """输入用户名"""
-        return self.input_username.fill("admin")
+        return self.input_username.fill(content)
 
     @logger
     @ec
-    def password_input(self):
+    def password_input(self, content: str):
         """输入密码"""
-        return self.input_password.fill("xz666666")
+        return self.input_password.fill(content)
+
+    @logger
+    @ec
+    def agreement_click(self):
+        """勾选用户协议"""
+        return self.cb_agreement.click()
 
     @logger
     @ec
     def login_click(self):
         """点击登录"""
         return self.bt_login.click()
+
+    @logger
+    @ec
+    def a_goto(self, nth):
+        """前往用户协议网页"""
+        return self.a.nth(nth).click()
