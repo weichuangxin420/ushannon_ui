d845bae51804e1a583f71354e3cdc6881bdf6fc7
chuangxinwei <18210880215@163.com>
2024-12-11 18:30:19 +0800

    增加了验证码用例

--- src/ushannon_ui/tests/test_login_page.py
+++ src/ushannon_ui/tests/test_login_page.py
@@ -1,20 +1,18 @@
 import time
+from random import randint
 
 import pytest
 
 from config.env import URLDve
-from src.utils.random_data_maker import  random_data_maker as rdm
-from random import randint
-
+from src.utils.random_data_maker import random_data_maker as rdm
 from utils.logger import log
 
 
-@pytest.mark.usefixtures("init_page")
 class TestLogin:
 
     def test_login(self, login_page):
         """测试登录"""
-        login_page[0].value_input({"username":"admin", "password":"xz666666"})
+        login_page[0].value_input({"username": "admin", "password": "xz666666"})
         login_page[0].bt_click("agreement")
         login_page[0].bt_click("login")
 
@@ -26,26 +24,29 @@ class TestLogin:
         except TimeoutError:
             assert False, "超时未跳转到指定url"
 
-            #由于登录后进入了主页，影响其他用例执行，需要返回登录页
+            # 由于登录后进入了主页，影响其他用例执行，需要返回登录页
         finally:
 
             # 清除 LocalStorage ，其中包含了token
             login_page[2].evaluate("localStorage.clear();")
             login_page[2].goto(URLDve.OJ_front)
 
-
-    @pytest.mark.parametrize("values,number",[
-        (rdm(11,False,True,True,True),"1"),
-        (rdm(randint(0,10),True,False,False,False),"2"),
-        (rdm(12,True,False,False,False),"3"),
-        (rdm(11,True,False,False,False),"4"),
-    ],ids = ["输入非数字字符","输入11以下的数字","输入12位数字","输入11位数字"])
-    def test_input(self,login_page,values,number):
+    @pytest.mark.parametrize(
+        "values,number",
+        [
+            (rdm(11, False, True, True, True), "1"),
+            (rdm(randint(0, 10), True, False, False, False), "2"),
+            (rdm(12, True, False, False, False), "3"),
+            (rdm(11, True, False, False, False), "4"),
+        ],
+        ids=["输入非数字字符", "输入11以下的数字", "输入12位数字", "输入11位数字"],
+    )
+    def test_phone_input(self, login_page, values, number):
         """测试输入框输入"""
-        #如果不是手机号登录，切换下手机号登录
+        # 如果不是手机号登录，切换下手机号登录
         if login_page[0].query_element("phone_login"):
             login_page[0].bt_click("phone_login")
-        login_page[0].value_input({"phone_number":values})
+        login_page[0].value_input({"phone_number": values})
         if number == "1":
             assert login_page[0].get_value("phone_number") == ""
         if number == "2":
@@ -56,11 +57,81 @@ class TestLogin:
             assert login_page[0].get_value("phone_number") == values
         login_page[0].input_clear("phone_number")
 
+    @pytest.mark.parametrize(
+        "phone_number,number",
+        [("17102108412", "1"), ("14000200000", "2"), ("17102108412", "3")],
+        ids=["无网获取验证码", "手机号未注册", "成功发送,倒计时后重新发送"],
+    )
+    def test_get_verifycode(self, login_page, phone_number, number):
+        """测试手机号输入框"""
 
+        # 每次执行恢复网络
+        login_page[1].set_offline(False)
+        # 如果不是手机号登录，切换下手机号登录
+        if login_page[0].query_element("phone_login", mode="visible"):
+            login_page[0].bt_click("phone_login")
+        # 输入手机号
+        login_page[0].value_input({"phone_number": phone_number})
 
+        # 断言断网吐司
+        if number == "1":
+            login_page[1].set_offline(True)
+            login_page[0].bt_click("get_verifycode")
+            assert login_page[0].query_element(
+                element="neterror", mode="wait_for_visible"
+            )
 
+        if number == "2":
+            login_page[0].bt_click("get_verifycode")
+            assert login_page[0].query_element(
+                element="nophone", mode="wait_for_visible"
+            )
 
-class TestGoto:
+        if number == "3":
+            login_page[0].bt_click("get_verifycode")
+            assert login_page[0].query_element(
+                element="success", mode="wait_for_visible"
+            )
+            assert login_page[0].query_element(element="timer", mode="wait_for_visible")
+            login_page[2].wait_for_timeout(60000)
+            assert login_page[0].query_element(
+                element="resend", mode="wait_for_visible"
+            )
+            login_page[0].bt_click("get_verifycode")
+            assert login_page[0].query_element(
+                element="success", mode="wait_for_visible"
+            )
+
+    @pytest.mark.parametrize(
+        "verifycode,number",
+        [
+            (rdm(6, False, True, True, True), "1"),
+            (rdm(randint(0, 5), True, False, False, False), "2"),
+            (rdm(7, True, False, False, False), "3"),
+            (rdm(6, True, False, False, False), "4"),
+        ],
+        ids=["输入非数字", "输入不足6位数", "输入7位数", "输入6位数"],
+    )
+    def test_verifycode_input(self, login_page, verifycode, number):
+        """测试验证码输入框"""
+
+        # 如果不是手机号登录，切换下手机号登录
+        if login_page[0].query_element("phone_login", mode="visible"):
+            login_page[0].bt_click("phone_login")
+        # 输入11位手机号
+        login_page[0].value_input({"phone_number": rdm(11, True, False, False, False)})
+        # 输入验证码
+        login_page[0].value_input({"verifycode": verifycode})
+        # 断言
+        if number == "1":
+            assert login_page[0].get_value("verifycode") == ""
+        if number == "2":
+            assert login_page[0].get_value("verifycode") == verifycode
+            assert not login_page[0].query_element(element="login", mode="enable")
+        if number == "3":
+            assert login_page[0].get_value("verifycode") == verifycode[:6]
+        if number == "4":
+            assert login_page[0].get_value("verifycode") == verifycode
 
     @pytest.mark.parametrize(
         "target_url,number",
@@ -81,7 +152,3 @@ class TestGoto:
         else:
             assert False, "new_pages 为空"
         org_page.bring_to_front()
-
-
-
-
