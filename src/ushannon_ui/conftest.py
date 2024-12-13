52a6fcc5417ff5ce73da426ec31877230106a558
chuangxinwei <18210880215@163.com>
2024-12-13 18:35:49 +0800

    增加失败直接跳过后续用例执行

--- src/ushannon_ui/conftest.py
+++ src/ushannon_ui/conftest.py
@@ -4,6 +4,28 @@ from playwright.sync_api import sync_playwright
 from src.config.env import URLDve
 from src.utils.logger import log
 
+failed_flag = False
+
+
+def pytest_runtest_makereport(item, call):
+    """
+    在每个阶段（setup、call、teardown）结束后触发，用于生成报告。
+    如果检测到测试用例失败，则设置全局失败标志。
+    """
+    global failed_flag
+    if call.when == "call" and call.excinfo is not None:  # 如果在测试用例执行阶段失败
+        failed_flag = True
+
+
+def pytest_runtest_setup(item):
+    """
+    在每个测试用例运行前触发，用于检查是否需要跳过测试。
+    如果发现之前有失败的用例，则跳过当前测试。
+    """
+    global failed_flag
+    if failed_flag:  # 如果发现之前的用例失败，跳过后续用例
+        pytest.skip(f"跳过：由于测试用例 '{item.name}' 之前有失败用例")
+
 
 @pytest.fixture(scope="class")
 def init_page():
