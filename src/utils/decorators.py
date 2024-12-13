62a1a193d725b509767f7fe79d6cfbfe27db62b2
chuangxinwei <18210880215@163.com>
2024-12-13 18:36:26 +0800

    修改基础装饰器，可以带参调用也可以不带参调用

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,38 +1,78 @@
 # 存放装饰器
 from functools import wraps
+from time import time
 
 from src.utils.logger import log
 
 
-def exception_catcher(func):
+def base_decorator(log_level):
     """捕获异常并记录日志的装饰器"""
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            # 记录错误日志，包含函数名称和异常信息
-            log.error(f"在执行 '{func.__name__}' 时捕获到异常：{e}", exc_info=True)
-            # 重新抛出异常以便上层调用者处理
+    # 判断是否直接调用
+    if callable(
+        log_level
+    ):  # 如果直接使用 @base_decorator，则 level 实际上是被装饰的函数
+        func = log_level  # 被装饰的函数
+        level = "debug"  # 默认日志等级
 
-    return wrapper
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                start_time = time()
+                log.debug(f"{func.__name__}开始执行")
+                result = func(*args, **kwargs)
+                end_time = time()
+                log.debug(f"{func.__name__}执行成功, 耗时{end_time - start_time:.2f}秒")
+                return result
+            except Exception as e:
+                log.error(f"在执行 '{func.__name__}' 时捕获到异常：{e}", exc_info=True)
+                raise
 
+        return wrapper
 
-def logger(func):
-    """记录日志的装饰器"""
+    # 等级映射
+    log_levels = {
+        "debug": log.debug,
+        "info": log.info,
+        "error": log.error,
+        "critical": log.critical,
+    }
 
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        log.debug(f"{func.__name__}执行成功")
-        return result
+    def actual_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                # 计算执行时间
+                start_time = time()
 
-    return wrapper
+                if log_level in log_levels:
+                    log_levels[log_level](f"{func.__name__}开始执行")
+                else:
+                    log.error("装饰器错误，输入的日志等级无效")
+                    raise "装饰器错误，输入的日志等级无效"
+
+                result = func(*args, **kwargs)
+                end_time = time()
+
+                log_levels[log_level](
+                    f"{func.__name__}执行成功,耗时{start_time-end_time:.2f}s"
+                )
+
+                return result
+            except Exception as e:
+                # 记录错误日志，包含函数名称和异常信息
+                log.error(f"在执行 '{func.__name__}' 时捕获到异常：{e}", exc_info=True)
+                # 重新抛出异常以便上层调用者处理
+                raise e
+
+        return wrapper
+
+    return actual_decorator
 
 
 if __name__ == "__main__":
     #
-    @logger
+    @base_decorator(log_level="info")
     # @UI_exception_catcher
     def tsum(a, b):
         return a + b
