a18a5a43a4c6154fbf0c1d01e7abf0ab4714079e
chuangxinwei <18210880215@163.com>
2024-12-13 15:14:46 +0800

    支持生成纯中文随机字符

--- src/utils/random_data_maker.py
+++ src/utils/random_data_maker.py
@@ -3,7 +3,7 @@ import string
 
 
 def random_data_maker(
-    length=5, integer=True, letter=False, punctuation=True, chinese=False
+    length=5, integer=True, letter=False, punctuation=False, chinese=False
 ):
     """用于生成随机的字母，数字，特殊符合的字符串"""
     char_pool = ""
@@ -13,9 +13,10 @@ def random_data_maker(
         char_pool += string.ascii_letters
     if punctuation:
         char_pool += string.punctuation
+
     if chinese:
         char_pool += "".join(
-            chr(random.randint(0x4E00, 0x9FFF)) for _ in range(length // 4)
+            chr(random.randint(0x4E00, 0x9FFF)) for _ in range(30)
         )
     if char_pool:
         return "".join(random.choice(char_pool) for i in range(length))
@@ -25,5 +26,5 @@ def random_data_maker(
 
 if __name__ == "__main__":
     print(
-        random_data_maker(20, integer=True, letter=True, punctuation=True, chinese=True)
+        random_data_maker(2,integer=False,chinese=True)
     )
